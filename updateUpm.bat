8e3e752a8aa73e0f3970e0ffe7e895d38be7e4b4
Wurui <smallcorpse.rui@qq.com>
2024-01-14 13:18:01 +0800

    Update updateUpm.bat

--- updateUpm.bat
+++ updateUpm.bat
@@ -1,8 +1,13 @@
 @echo on
 
 git subtree split --prefix=Packages/com.capua.a2wtoolbox --branch upm
+
 git remote rm upm
 git remote add upm https://gitee.com/smallcorpse_rui/A2WToolbox.git
+git push -f upm upm:main
+
+git remote rm upm
+git remote add upm https://github.com/smallcorpse/A2WToolbox.git
 git push -f upm upm:main
 
 pause
